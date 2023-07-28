364148fbae750f276d658a59f9cce9567b0a1ca1
KuzAnton8335 <ant2783@gmail.com>
2023-07-28 13:45:29 +0300

    calculate form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 // подключение сервера с данными
 const API_URL = "https://tarry-juniper-chanter.glitch.me/";
 
+// переменная стоимости товара
+const price = {
+	Клубника: 60,
+	Киви: 55,
+	Банан: 50,
+	Маракуйя: 90,
+	Манго: 70,
+	Яблоко: 45,
+	Мята: 50,
+	Лед: 10,
+	Биоразлагаемый: 20,
+	Пластиковый: 0,
+};
+
+
+
 // функция подключения k данным на сервере
 const getData = async () => {
 	// const response = await fetch(API_URL + "api/goods");
@@ -31,7 +47,7 @@ const creatCard = (item) => {
 			<p class="coctail__price text-red">${item.price}</p>
 			<p class="coctail__size">${item.size}</p>
 		</div>
-		<button class="btn coctail__btn" type="button" data-id = "${item.id}">
+		<button class="btn coctail__btn coctail__btn-add" type="button" data-id = "${item.id}">
 			Добавить
 		</button>
 	
@@ -65,16 +81,85 @@ const scrollServices = {
 
 }
 
+
+// получение данных из формы для калькулятора
+const getFormData = (form) => {
+	const formData = new FormData(form);
+	const data = {};
+	for (const [name, value] of (formData.entries())) {
+		if (data[name]) {
+			if (!Array.isArray(data[name])) {
+				data[name] = [data[name]]
+			}
+			data[name].push(value)
+		} else {
+			data[name] = value;
+		}
+	}
+
+	return data;
+}
+
+
+
+// функция калькулятора подсчет формы состовляющей коктейль
+const calculateTotalPrice = (form, startPrice) => {
+	let totalPrice = startPrice;
+	const data = getFormData(form);
+
+	if (Array.isArray(data.ingredients)) {
+		data.ingredients.forEach(item => {
+			totalPrice += price[item] || 0
+		})
+	}
+	else {
+		totalPrice += price[data.ingredients] || 0
+	}
+
+	if (Array.isArray(data.toppings)) {
+		data.ingredients.forEach(item => {
+			totalPrice += price[item] || 0
+		})
+	}
+	else {
+		totalPrice += price[data.toppings] || 0
+	}
+
+	totalPrice += price[data.cup] || 0;
+
+	return totalPrice
+};
+
+
+// данные для калькулятора 
+const calculateMakeYuorOwn = () => {
+	const formMakeOwn = document.querySelector('.make__form-your-own');
+	const makeInputPrice = formMakeOwn.querySelector('.maik__input-price');
+	const makeTotalPrice = formMakeOwn.querySelector('.make__total-price');
+
+	const hendlerChange = () => {
+		const totalPrice = calculateTotalPrice(formMakeOwn, 150);
+		makeInputPrice.value = totalPrice;
+		makeTotalPrice.textContent = `${totalPrice} ₽`;
+	}
+
+	formMakeOwn.addEventListener('change', hendlerChange);
+	hendlerChange();
+};
+
 // функция получения данных 
 const init = async () => {
 	modalController({
 		modal: '.modal__order', btnOpen: '.header__btn-order',
 	});
 
+	calculateMakeYuorOwn();
+
 	modalController({
-		modal: ".modal__make",
+		modal: ".modal__make-your-own",
 		btnOpen: ".coctail__btn-make",
-	})
+	});
+
 	const goodsListElem = document.querySelector('.goods__list');
 	const data = await getData();
 
@@ -86,11 +171,18 @@ const init = async () => {
 	})
 
 	goodsListElem.append(...cartsCoctail)
+
+
+
+	modalController({
+		modal: '.modal__add',
+		btnOpen: '.coctail__btn-add'
+	})
 }
 
 // функция модального окна
 const modalController = ({ modal, btnOpen, time = 300 }) => {
-	const buttomElem = document.querySelector(btnOpen);
+	const buttomElems = document.querySelectorAll(btnOpen);
 	const modalElem = document.querySelector(modal);
 
 	modalElem.style.cssText = `
@@ -126,7 +218,9 @@ const modalController = ({ modal, btnOpen, time = 300 }) => {
 	}
 
 	// событие click модального окна
-	buttomElem.addEventListener('click', openModal);
+	buttomElems.forEach(buttomElem => {
+		buttomElem.addEventListener('click', openModal);
+	})
 	modalElem.addEventListener('click', closeModal);
 
 	return { openModal, closeModal };
